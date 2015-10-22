cd3934e7d686cc87a92e163ce5c40cd23356db5b
sanjoy <sanjoy120@gmail.com>
2015-10-22 16:55:41 +0600

    update

--- src/main/java/demo/UIApplication.java
+++ src/main/java/demo/UIApplication.java
@@ -2,9 +2,19 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -13,17 +23,48 @@ import java.util.UUID;
  * Created by sanjoy on 10/21/15.
  */
 @SpringBootApplication
+@EnableRedisHttpSession
 @RestController
 public class UIApplication {
     @RequestMapping("/resource")
-    public Map<String, Object> home(){
+    public Map<String, Object> home() {
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("id", UUID.randomUUID());
         model.put("content", "Hello World!");
         return model;
     }
 
-    public static void main(String[] args){
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(UIApplication.class, args);
     }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+                    .csrf().csrfTokenRepository(csrfTokenRepository());
+        }
+
+        private CsrfTokenRepository csrfTokenRepository() {
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setHeaderName("X-XSRF-TOKEN");
+            return repository;
+        }
+    }
+
+
 }
